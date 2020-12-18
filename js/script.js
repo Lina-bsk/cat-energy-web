6b7ee4d10353fcc490f2efc862bdb46d212a5254
Lina <z-lina@mail.ru>
2020-12-19 01:53:00 +0200

    htmlcss

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var menu = document.querySelector('.menu');
+var menuToggle = document.querySelector('.menu__toggle');
+
+menu.classList.remove('menu--nojs');
+
+menuToggle.addEventListener('click', function() {
+  if (menu.classList.contains('menu--closed')) {
+    menu.classList.remove('menu--closed');
+    menu.classList.add('menu--opened');
+  } else {
+    menu.classList.add('menu--closed');
+    menu.classList.remove('menu--opened');
+  }
+});
